716871cf4853006e7a421dec8d9523253c2192d1
Phillip Kheyfets <phillipkheyfets@gmail.com>
2017-02-18 20:59:25 -0500

    chapter 2 solutions

--- 2.4/constants.h
+++ 2.4/constants.h
@@ -1,11 +1,3 @@
-//
-//  constants.h
-//  2.4
-//
-//  Created by Phillip Kheyfets on 2/18/17.
-//  Copyright © 2017 Phillip Kheyfets. All rights reserved.
-//
-
 #ifndef constants_h
 #define constants_h
 

ea3503567e61ef2b1f34b2a2f225872be5321f06
Phillip Kheyfets <phillipkheyfets@gmail.com>
2017-03-03 23:28:07 -0500

    chapter 6.2

--- 6.2/6.2/main.cpp
+++ 6.2/6.2/main.cpp
@@ -1,15 +1,44 @@
-//
-//  main.cpp
-//  6.2
-//
-//  Created by Phillip Kheyfets on 3/3/17.
-//  Copyright © 2017 Phillip Kheyfets. All rights reserved.
-//
-
 #include <iostream>
+#include <string>
+#include <utility>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+struct Student {
+    std::string name;
+    int grade;
+};
+
+void sortNames(Student *students, int length) {
+    for (int i = 0; i < length; i++) {
+        int largest = i;
+        
+        for (int j = i + 1; j < length; j++) {
+            if (students[j].grade > students[largest].grade) {
+                largest = j;
+            }
+        }
+
+        std::swap(students[i], students[largest]);
+    }
+}
+
+int main() {
+    int numStudents = 0;
+    std::cout << "How many students do you want to enter? ";
+    std::cin >> numStudents;
+    
+    Student *students = new Student[numStudents];
+    
+    for (int i = 0; i < numStudents; i++) {
+        std::cout << "Enter name #" << i + 1 << ": ";
+        std::cin >> students[i].name;
+        
+        std::cout << "Enter grade #" << i + 1 << ": ";
+        std::cin >> students[i].grade;
+    }
+    
+    sortNames(students, numStudents);
+    
+    for (int i = 0; i < numStudents; i++) {
+        std::cout << students[i].name << " got a grade of " << students[i].grade << std::endl;
+    }
 }
